945347694abb6f0f1bb58e297627a109a52b2608
lijiahao555 <787660079@qq.com>
2018-03-12 21:42:28 +0800

    公共视图文件

--- mvc/corephp/base/Controller.php
+++ mvc/corephp/base/Controller.php
@@ -10,13 +10,15 @@ class Controller
     protected $_controller;
     protected $_action;
     protected $_view;
+    public $layout;
 
     // 构造函数，初始化属性，并实例化对应模型
     public function __construct($controller, $action)
     {
+
         $this->_controller = $controller;
         $this->_action = $action;
-        $this->_view = new View($controller, $action);
+        $this->_view = $this->layout ? new View($controller, $action, $this->layout) : new View($controller, $action);
     }
 
     // 分配变量
